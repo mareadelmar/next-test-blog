b28b80a163e1d78aa49d2c53627fd713f7f26e70
mareadelmar <mar.ortega.v@gmail.com>
2021-11-25 11:24:36 -0300

    Connect post component with function that pass data

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,16 @@
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 
-export default function Post() {
-	return <Layout>...</Layout>;
+export default function Post({ postData }) {
+	return (
+		<Layout>
+			{postData.title}
+			<br />
+			{postData.id}
+			<br />
+			{postData.date}
+		</Layout>
+	);
 }
 
 export async function getStaticPaths() {
